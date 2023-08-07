49904b552bab8e72541400a36d974816fd25bad9
Ravinagala <nagalaravich04182@gmail.com>
2023-08-07 14:13:35 +0530

    one file added

--- StringArray.java
+++ StringArray.java
@@ -1,4 +1,19 @@
 package ravi_intellij;
 
+import java.util.Scanner;
+
 public class StringArray {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of names you want to enter:");
+        int num = sc.nextInt();
+        String[] names = new String[num];
+        for(int i = 0; i<names.length;i++){
+            names[i] = sc.next();
+        }
+
+        for(int i = names.length - 1; i >= 0; i--){
+            System.out.println(names[i]);
+        }
+    }
 }
