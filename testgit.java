c7a62d10173d316fc9d6aae70da754b030242d9d
thesriharsha <sri235760@gmail.com>
2023-08-09 09:53:35 +0530

    Test IntelliJ

---
+++ testgit.java
@@ -0,0 +1,7 @@
+package ravi_intellij;
+
+public class testgit {
+    public static void main(String[] args) {
+        System.out.println("Hey there this is to test the push option in IntelliJ");
+    }
+}
